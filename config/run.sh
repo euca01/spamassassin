0851e3fd2232fef2bb0fbf1dd0b899b762df8b54
Nicolas BERNA <nicolas_github@berna.fr>
2023-11-26 00:40:24 +0100

    test

--- config/run.sh
+++ config/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Personnalisation de la configuration
+
 #sed -i "s/#connect =/connect = host=${DB_HOST} dbname=${DB_DATABASE} user=${DB_USER} password=${DB_PASSWORD}/g" /etc/dovecot/dovecot-sql.conf.ext
 
 #Start services
